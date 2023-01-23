34271e81a67f6ac0a9d99c61f5b3d6091528614d
Alvaro A Miranda <45268088+varitomirandacr@users.noreply.github.com>
2023-01-22 19:13:37 -0600

    Final Sunday

--- Amount.java
+++ Amount.java
@@ -1,3 +1,49 @@
+/*
+ * This assignment is based on Erik Poll's assignment (Radboud University, Nijmegen).
+ */
+
+/* OpenJML exercise
+
+   Objects of this class represent euro amounts. For example, an Amount 
+   object with
+     euros == 1
+     cents == 55
+   represents 1.55 euro. 
+
+   Specify the class with JML and check it OpenJML.  
+
+   NB there may be errors in the code that you have to fix to stop 
+   OpenJML from complaining, as these complaints of OpenJML 
+   point to real bugs in the code. But keep changes to the code to
+   a minimum of what is strictly needed. 
+   Mark any changes you have made in the code with a comment,
+   eg to indicate that you replaced <= by <.
+
+   You should add enough annotations to stop OpenJML complaining,
+   but you should ALSO specify invariants discussed below:
+
+   1) We do not want to represent 1.55 euro as an object with
+         euro  == 0
+         cents == 155
+      (Note that the "equals" method will not be correct if we allow 
+       this.)
+      Specify an invariant that rules this out.
+
+   2) We do not want to represent 1.55 euro as an object with
+         euros =  2
+         cents =  -45
+      Specify one (or more) invariant(s) that rule this out. But note that
+      we DO want to allow negative amounts, otherwise the method negate 
+      can't be allowed.
+      It may be useful to use the JML notation ==> (for implication) in 
+      your invariants.
+
+   While developing your specs, it may be useful to use the keywords
+      assert
+   to add additional assertions in source code, to find out what 
+   OpenJML can - or cannot - prove at a given program point.
+
+*/
 
 public class Amount{
 
